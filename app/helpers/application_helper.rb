f5bf832157c670950bd5d8dd1ceef52c96742681
Kirill Pimenov <kirill@pimenov.cc>
2024-05-03 01:34:16 +0200

    Limit the height of the teams' agendas in the list as well

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,8 @@ module ApplicationHelper
   end
 
   def format_markdown(source)
+    return '' if source.blank?
+
     content = Commonmarker.to_html(source, options:{ parse: { smart: true }, render: { escape: true }})
     sanitized_content = sanitize(content, tags: %w(h1 h2 h3 h4 h5 h6 p a ul ol li table tr td th input img blockquote br pre span hr em strong del), attributes: %w(href src type style checked disabled))
     sanitized_content.gsub(/<a href="http[^"]*"/) { |match| match.gsub('<a href="', '<a target="_blank" href="') }

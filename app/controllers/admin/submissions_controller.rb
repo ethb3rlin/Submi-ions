d05c6b2e6963e917d370fd7570031b7f66877a93
Kirill Pimenov <kirill@pimenov.cc>
2024-05-04 02:42:06 +0200

    Make submissions filterable by track in admin UI

--- app/controllers/admin/submissions_controller.rb
+++ app/controllers/admin/submissions_controller.rb
@@ -1,6 +1,10 @@
 class Admin::SubmissionsController < ApplicationController
   def index
-    @submissions = Submission.all
+    @submissions = if params[:track].present? && Submission::HUMAN_READABLE_TRACKS[params[:track]].present?
+      Submission.where(track: params[:track])
+    else
+      Submission.all
+    end
     authorize @submissions
 
     @unassigned_count = Submission.unassigned.count
